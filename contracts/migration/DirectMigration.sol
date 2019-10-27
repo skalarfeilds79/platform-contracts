c4c571f4a66a26fc2d43fd8baf769cd4bb8fb3bd
Alex Connolly <alex@immutable.com>
2019-10-27 19:37:37 +1100

    updating direct migration to reflect new proto APIs

--- contracts/migration/DirectMigration.sol
+++ contracts/migration/DirectMigration.sol
@@ -51,7 +51,7 @@ contract DirectMigration {
 
         for (uint i = 0; i < size; i++) {
             (proto, purity) = old.getCard(start+i);
-            protos[i] = proto;
+            protos[i] = convertProto(proto);
             qualities[i] = convertPurity(purity);
         }
 
@@ -71,6 +71,53 @@ contract DirectMigration {
 
     function convertPurity(uint16 purity) public pure returns (uint8) {
         return uint8((purity / 1000) + 2);
+    }
+
+    function convertProto(uint16 proto) public view returns (uint16) {
+        if (proto >= 1 && proto <= 377) {
+            return proto;
+        }
+        // first phoenix
+        if (proto == 380) {
+            return 400;
+        }
+        // chimera
+        if (proto == 394) {
+            return 401;
+        }
+        // etherbots
+        (bool found, uint index) = getEtherbotsIndex(proto);
+        if (found) {
+            return uint16(380 + index);
+        }
+        // hyperion
+        if (proto == 378) {
+            return 65000;
+        }
+        // prometheus
+        if (proto == 379) {
+            return 65001;
+        }
+        // atlas
+        if (proto == 383) {
+            return 65002;
+        }
+        // tethys
+        if (proto == 384) {
+            return 65003;
+        }
+        require(false, "unrecognised proto");
+    }
+
+    uint16[] internal ebs = [400, 413, 414, 421, 427, 428, 389, 415, 416, 422, 424, 425, 426, 382, 420, 417];
+
+    function getEtherbotsIndex(uint16 proto) public view returns (bool, uint16) {
+        for (uint16 i = 0; i < ebs.length; i++) {
+            if (ebs[i] == proto) {
+                return (true, i);
+            }
+        }
+        return (false, 0);
     }
 
 }
--- deployment/deploy.js
+++ deployment/deploy.js
@@ -8,7 +8,7 @@ const DeployManager = require('../util/deploy-manager.js');
 
 const deploy = async (network, secret, etherscanApiKey) => {
 
-	const manager = new DeployManager(network, secrets.privateKey);
+	const manager = new DeployManager(network, secrets.networks.mainnet.privateKey);
 
 	deployer = manager.getDeployer();
 
@@ -18,13 +18,18 @@ const deploy = async (network, secret, etherscanApiKey) => {
 
 	let cards = await deployer.deployAndVerify(Cards, {}, BATCH_SIZE, "Gods Unchained Cards", "CARD");
 
-	let minter = await deployer.deployAndVerify(OpenMinter, {}, cards.contractAddress);
+	let direct = await deployer.deployAndVerify(DirectMigration, {}, cards.contractAddress);
 
-	await cards.startSeason(0, 377);
-	await cards.addFactory(minter.contractAddress, 0);
-	await cards.unlockTrading(0);
+	await cards.startSeason(0, 381);
+	await cards.addFactory(direct.contractAddress, 0);
 	
 
+	//let minter = await deployer.deployAndVerify(OpenMinter, {}, cards.contractAddress);
+
+	/*	await cards.startSeason(0, 377);
+		await cards.addFactory(minter.contractAddress, 0);
+		await cards.unlockTrading(0);
+	*/
 };
 
 module.exports = {

de5c4ecc8de670c6d4f026153e13e005a951d759
Kerman Kohli <kerman@Kermans-MacBook-Pro.local>
2019-10-31 13:47:35 +1100

    Test infrastructure is up!

---
+++ __tests__/core.test.ts
@@ -0,0 +1,17 @@
+import { BigNumber } from 'ethers/utils';
+
+import {
+    CardsFactory, Cards
+} from '../types'
+
+import { ethers, Wallet } from 'ethers';
+
+describe('Core', () => {
+
+    let core: Cards;
+
+    it('should run', () => {
+        expect(true);
+    })
+
+});

40d25b09fda79281d24131789a673df25ba67143
Kerman Kohli <kermankohli@gmail.com>
2019-11-21 16:46:10 +1100

    Updated forge contract

--- packages/contracts/contracts/cards/factories/Forge.sol
+++ packages/contracts/contracts/cards/factories/Forge.sol
@@ -1,11 +1,19 @@
 pragma solidity 0.5.11;
 
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+
 import "../interfaces/ICards.sol";
 
-contract Forge {
+contract Forge is Ownable {
 
     ICards cards;
+
     uint8 constant diamond = 1;
+
+    bool public locked = false;
+
+    event LockSet(bool status);
+    event ForgedCards(uint finalId, uint[] all);
 
     constructor(ICards _cards) public {
         cards = _cards;
@@ -13,12 +21,22 @@ contract Forge {
 
     function forge(uint[] memory _ids) public {
         require(
+            locked == false,
+            "Forge: contract must be unlocked"
+        );
+
+        require(
             _ids.length == 5,
             "Forge: must forge 5 at a time"
         );
 
         (uint16 proto, uint8 quality) = cards.getDetails(_ids[0]);
         address owner = cards.ownerOf(_ids[0]);
+
+        require(
+            owner == msg.sender || cards.isApprovedForAll(owner, msg.sender) == true,
+            "Forge: cannot forge without authorisation"
+        );
 
         require(
             quality > diamond,
@@ -47,6 +65,16 @@ contract Forge {
         }
 
         cards.setQuality(_ids[0], quality - 1);
+
+        emit ForgedCards(_ids[0], _ids);
+    }
+
+    function setLock(bool status)
+        onlyOwner
+        public
+    {
+        locked = status;
+        emit LockSet(status);
     }
 
 }

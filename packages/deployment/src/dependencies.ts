7a7fabd877d3cd343d68b2f93eb947f03b10db01
Kerman Kohli <kermankohli@gmail.com>
2019-11-21 17:18:07 +1100

    Deployed new forwarder

--- packages/deployment/src/dependencies.ts
+++ packages/deployment/src/dependencies.ts
@@ -18,15 +18,19 @@ export default {
       50: 'test-rpc'
     },
     WETH: {
+        1: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
         3: "0xc778417e063141139fce010982780140aa0cd5ab"
     },
     ZERO_EX_EXCHANGE: {
+        1: "0x080bf510fcbf18b91105470639e9561022937712",
         3: "0xbff9493f92a3df4b0429b6d00743b3cfb4c85831"
     },
     ZERO_EX_ERC20_PROXY: {
+        1: "0x95e6f48254609a6ee006f7d493c8e5fb97094cef",
         3: "0xb1408f4c245a23c31b98d2c626777d4c0d766caa"
     },
     ZERO_EX_ERC721_PROXY: {
+        1: "0xefc70a1b18c432bdc64b596838b4d138f6bc6cad",
         3: "0xe654aac058bfbf9f83fcaee7793311dd82f6ddb4"
     }
 };

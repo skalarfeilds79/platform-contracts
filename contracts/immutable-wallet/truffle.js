fe5706f00b729cb756f8b7800973ed8c010f1313
Kerman Kohli <kermankohli@gmail.com>
2019-11-27 12:07:05 +1100

    Restructured contracts and removed legacy cards/migration code

---
+++ contracts/immutable-wallet/truffle.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+const config = dotenv.config({path: '../../.env'}).parsed;
+
+const truffleConfig = require('../../truffle.config');
+
+module.exports = {
+  ...truffleConfig,
+  api_keys: {
+    etherscan: config.ETHERSCAN_KEY
+  }
+};

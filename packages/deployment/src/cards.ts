c9655547ad8de7d3000ca11b23df68de2bd7fcd8
Kerman Kohli <kermankohli@gmail.com>
2019-11-28 16:35:32 +1100

    Test setup finished?

--- packages/deployment/src/cards.ts
+++ packages/deployment/src/cards.ts
@@ -1,13 +1,19 @@
 import { CardsFactory, OpenMinterFactory} from '@imtbl/gods-unchained';
 import { ethers } from 'ethers';
-import { writeContractToOutputs, writeStateToOutputs } from './utils/outputHelpers';
+import { writeContractToOutputs, writeStateToOutputs, getNetworkId } from './utils/outputHelpers';
 
 const BATCH_SIZE = 1251;
 
 const dotenv = require('dotenv');
 const config = dotenv.config({path: '../../.env'}).parsed;
 
-const provider = new ethers.providers.JsonRpcProvider(config.RPC_ENDPOINT, config.DEPLOYMENT_NETWORK_ID);
+const networkId = getNetworkId()
+
+let provider = new ethers.providers.JsonRpcProvider(config.RPC_ENDPOINT, networkId);
+if (networkId == 50) {
+  provider = new ethers.providers.JsonRpcProvider(config.RPC_ENDPOINT);
+}
+
 const wallet = new ethers.Wallet(config.PRIVATE_KEY, provider);
 
 async function deploy() {
@@ -19,21 +25,28 @@ async function deploy() {
     );
 
     console.log('Starting Seasons...')
-    await cards.functions.startSeason("Genesis", 1, 377, {gasLimit: 1000000});
-	await cards.functions.startSeason("Etherbots", 380, 396, {gasLimit: 1000000});
-    await cards.functions.startSeason("Promo", 400, 500, {gasLimit: 1000000});
+    const startSeason1 = await cards.functions.startSeason("Genesis", 1, 377, {gasLimit: 1000000});
+    await startSeason1.wait();
+
+    const startSeason2 = await cards.functions.startSeason("Etherbots", 380, 396, {gasLimit: 1000000});
+    await startSeason2.wait();
+
+    const startSeason3 = await cards.functions.startSeason("Promo", 400, 500, {gasLimit: 1000000});
+    await startSeason3.wait();
 
     console.log('Deploying OpenMinter...')
     const minter = await new OpenMinterFactory(wallet).deploy(cards.address);
 
     console.log('Adding Factory...')
-    await cards.functions.addFactory(minter.address, 1);
+    const addFactory = await cards.functions.addFactory(minter.address, 1);
+    await addFactory.wait();
 
     console.log('Unlocking Trading...')
-    await cards.functions.unlockTrading(1);
+    const unlockTrading = await cards.functions.unlockTrading(1);
+    await unlockTrading.wait();
 
     await writeContractToOutputs('Cards', cards.address);
-    await writeContractToOutputs('OpenMinter', cards.address);
+    await writeContractToOutputs('OpenMinter', minter.address);
 
     return `Cards: ${cards.address}\nMinter: ${minter.address}`;
 
--- packages/deployment/src/wallet.ts
+++ packages/deployment/src/wallet.ts
@@ -1,6 +1,24 @@
-import { WalletFactory, RegistryFactory, LockLimiterFactory, BackupModuleFactory, RecoveryModuleFactory, TransferModuleFactory, LimitedModulesFactory, SimpleDelegateFactory, MultiLimiterFactory, ProxyFactory, FactoryFactory} from '@imtbl/immutable-wallet';
+import { 
+  WalletFactory, 
+  RegistryFactory, 
+  LockLimiterFactory, 
+  BackupModuleFactory, 
+  RecoveryModuleFactory, 
+  TransferModuleFactory, 
+  LimitedModulesFactory, 
+  SimpleDelegateFactory, 
+  MultiLimiterFactory, 
+  ProxyFactory, 
+  FactoryFactory,
+  PurchaseModuleFactory
+} from '@imtbl/immutable-wallet';
+
+import { generatedWallets } from '@imtbl/test-utils';
+
 import { ethers } from 'ethers';
-import { getNetworkId, writeContractToOutputs } from './utils/outputHelpers';
+import { getNetworkId, writeContractToOutputs, getContractAddress, returnOutputs } from './utils/outputHelpers';
+
+import { deploy as deployForwarder } from './forwarder';
 
 const dotenv = require('dotenv');
 const config = dotenv.config({path: '../../.env'}).parsed;
@@ -11,36 +29,49 @@ const SECURITY_WINDOW = 100000;
 const RECOVERY_PERIOD = 100;
 const LOCK_PERIOD = 100;
 
-const provider = new ethers.providers.JsonRpcProvider(config.RPC_ENDPOINT, getNetworkId());
-const wallet = new ethers.Wallet(config.PRIVATE_KEY, provider);
+const networkId = getNetworkId()
+
+let provider = new ethers.providers.JsonRpcProvider(config.RPC_ENDPOINT, networkId);
+if (networkId == 50) {
+  provider = new ethers.providers.JsonRpcProvider(config.RPC_ENDPOINT);
+}
+
+let signer = provider.getSigner();
 
 async function deploy() {
 
-  const walletFactory = await new WalletFactory(wallet).deploy();
-  await writeContractToOutputs('Wallet', walletFactory.address);
+  const dummyWallet = await new WalletFactory(signer).deploy();
  
-  const registry = await new RegistryFactory(wallet).deploy(DELAY);
+  const registry = await new RegistryFactory(signer).deploy(DELAY);
   await writeContractToOutputs('Registry', registry.address);
 
-  const lock = await new LockLimiterFactory(wallet).deploy();
+  const lock = await new LockLimiterFactory(signer).deploy();
   await writeContractToOutputs('LockLimiter', lock.address);
 
-  const backupModule = await new BackupModuleFactory(wallet).deploy(SECURITY_PERIOD, SECURITY_WINDOW);
+  const backupModule = await new BackupModuleFactory(signer).deploy(SECURITY_PERIOD, SECURITY_WINDOW);
   await writeContractToOutputs('BackupModule', backupModule.address);
 
-  const recovery = await new RecoveryModuleFactory(wallet).deploy(
+  let forwarderAddress = await getContractAddress('Forwarder');
+  if (!forwarderAddress) {
+    forwarderAddress = await deployForwarder();
+  }
+
+  const purchaseModule = await new PurchaseModuleFactory(signer).deploy(forwarderAddress);
+  await writeContractToOutputs('PurchaseModule', purchaseModule.address);
+
+  const recoveryModule = await new RecoveryModuleFactory(signer).deploy(
     lock.address,
     backupModule.address,
     RECOVERY_PERIOD,
     LOCK_PERIOD
   );
-  await writeContractToOutputs('RecoveryModule', recovery.address);
+  await writeContractToOutputs('RecoveryModule', recoveryModule.address);
 
-  const transferModule = await new TransferModuleFactory(wallet).deploy();
+  const transferModule = await new TransferModuleFactory(signer).deploy();
   await writeContractToOutputs('TransferModule', transferModule.address);
 
   await registry.functions.register(
-    recovery.address,
+    recoveryModule.address,
     ethers.utils.formatBytes32String('Recovery')
   );
 
@@ -54,25 +85,39 @@ async function deploy() {
     ethers.utils.formatBytes32String('Transfer')
   );
 
-  const modules = await new LimitedModulesFactory(wallet).deploy(registry.addresss);
+  const modules = await new LimitedModulesFactory(signer).deploy(registry.address);
   await writeContractToOutputs('LimitedModule', modules.address);
 
-  const delegates = await new SimpleDelegateFactory(wallet).deploy();
+  const delegates = await new SimpleDelegateFactory(signer).deploy();
   await writeContractToOutputs('SimpleDelegate', delegates.address);
 
-  const limiter = await new MultiLimiterFactory(wallet).deploy();
+  const limiter = await new MultiLimiterFactory(signer).deploy();
   await writeContractToOutputs('MultiLimiter', limiter.address);
 
   const bytecode =
-  `${new ProxyFactory().bytecode}${ethers.utils.defaultAbiCoder.encode(['address'], [wallet.address]).slice(2)}`
+  `${new ProxyFactory().bytecode}${ethers.utils.defaultAbiCoder.encode(['address'], [dummyWallet.address]).slice(2)}`
 
-  const factory = await new FactoryFactory(wallet).deploy(
+  const factory = await new FactoryFactory(signer).deploy(
     modules.address,
     delegates.address,
     limiter.address,
     bytecode
   );
-  await writeContractToOutputs('Factory', factory.address);
+  await writeContractToOutputs('WalletFactory', factory.address);
+
+  if (networkId == 50) {
+    const userWallet = generatedWallets(provider)[1]
+    const x = await new FactoryFactory(userWallet).attach(factory.address).functions.createProxyWallet(
+      userWallet.address,
+      [backupModule.address, transferModule.address],
+      [],
+      {
+        gasLimit: 2000000
+      }
+    );
+    await x.wait()
+    console.log('*** Deployed contract wallet ***');
+  }
 
 }
 

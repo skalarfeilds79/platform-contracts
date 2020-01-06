fbfc35acda7ad9559e9e16412e18847bbc01b935
Kerman Kohli <kermankohli@gmail.com>
2020-01-06 13:49:06 +1100

    Chimera and etherbots migration outline completed

---
+++ contracts/gods-unchained/contracts/migration/EtherbotsMigration.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.5.11;
+
+contract EtherbotsMigration {
+
+    // Keep track of migrated cards
+    mapping (uint => bool) hasMigrated;
+
+    /**
+     * @dev Migrate Etherbot cards from the old Cards contract to the new one.
+     *
+     * @param _cardId The id of the Etherbots card
+     */
+    function migrate(
+        uint _cardId
+    ) public {}
+}

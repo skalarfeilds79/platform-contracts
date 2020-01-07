077a4a079a81d9c473985d950433b323682708a1
Kerman Kohli <kermankohli@gmail.com>
2020-01-07 16:57:20 +1100

    Chimera migration tests passing!

--- contracts/legacy-gods-unchained/contracts/migration/ChimeraMigration.sol
+++ contracts/legacy-gods-unchained/contracts/migration/ChimeraMigration.sol
@@ -1,14 +1,72 @@
 pragma solidity 0.5.11;
 
-contract ChimeraMigration {
+import "./BaseMigration.sol";
+import "../cards/CardIntegrationTwo.sol";
+import "../cards/CardBase.sol";
+import "@imtbl/gods-unchained/contracts/factories/PromoFactory.sol";
+
+contract ChimeraMigration is BaseMigration {
+
+    // The old cards contract
+    CardIntegrationTwo public oldCards;
+
+    // The new cards contract to migrate to
+    PromoFactory public promoFactory;
+
+    // The cut off point at which before cards will NOT be migrated.
+    uint public cutOffLimit;
 
     // Keep track of migrated and claimed chimeras
-    mapping (uint => bool) hasMigrated;
+    mapping (uint => bool) public hasMigrated;
+
+    event ChimeraMigrated(
+        uint tokenId,
+        address owner,
+        uint16 proto,
+        uint8 quality
+    );
+
+    constructor(
+        address _oldCardsAddress,
+        address _promoFactoryAddress,
+        uint _cutOffLimit
+    )
+        public
+    {
+        oldCards = CardIntegrationTwo(_oldCardsAddress);
+        promoFactory = PromoFactory(_promoFactoryAddress);
+        cutOffLimit = _cutOffLimit;
+    }
 
     /**
      * @dev Migrate Chimeras from the old Cards contract to the new one.
      */
     function migrate(
         uint _tokenId
-    ) public {}
+    ) public {
+        require(
+            _tokenId >= cutOffLimit,
+            "Chimera Migration: must be greater than the cut off"
+        );
+
+        require(
+            oldCards.ownerOf(_tokenId) == msg.sender,
+            "Chimera Migration: must be the owner of the Chimera"
+        );
+
+        require(
+            hasMigrated[_tokenId] == false,
+            "Chimera Migration: has already migrated Chimera"
+        );
+
+        (uint16 proto, uint16 purity) = oldCards.getCard(_tokenId);
+
+        uint16 convertedProto = convertProto(proto);
+        uint8 convertedQuality = convertPurity(purity);
+
+        hasMigrated[_tokenId] = true;
+        promoFactory.mintSingle(msg.sender, convertedProto, convertedQuality);
+
+        emit ChimeraMigrated(_tokenId, msg.sender, convertedProto, convertedQuality);
+    }
 }
--- contracts/legacy-gods-unchained/contracts/migration/EtherbotsMigration.sol
+++ contracts/legacy-gods-unchained/contracts/migration/EtherbotsMigration.sol
@@ -3,7 +3,7 @@ pragma solidity 0.5.11;
 contract EtherbotsMigration {
 
     // Keep track of migrated cards
-    mapping (uint => bool) hasMigrated;
+    mapping (uint => bool) public hasMigrated;
 
     /**
      * @dev Migrate Etherbot cards from the old Cards contract to the new one.

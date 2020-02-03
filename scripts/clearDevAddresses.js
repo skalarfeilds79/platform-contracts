8255c17de6c1247cd139115d5211eb9a26c5b3c5
Kerman Kohli <kermankohli@gmail.com>
2020-02-04 10:27:58 +1100

    Uncomment clear script

--- scripts/clearDevAddresses.js
+++ scripts/clearDevAddresses.js
@@ -1,15 +1,15 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// const OUTPUTS_PATH = './packages/addresses/src';
-// const outputs = JSON.parse(fs.readFileSync(`${OUTPUTS_PATH}/outputs.json`));
+const OUTPUTS_PATH = './packages/addresses/src';
+const outputs = JSON.parse(fs.readFileSync(`${OUTPUTS_PATH}/outputs.json`));
 
-// Object.keys(outputs)
-//   .filter((key) => key.includes('50'))
-//   .forEach((key) => (outputs[key] = undefined));
+Object.keys(outputs)
+  .filter((key) => key.includes('50'))
+  .forEach((key) => (outputs[key] = undefined));
 
-// const outputsJson = JSON.stringify(outputs, null, 2);
+const outputsJson = JSON.stringify(outputs, null, 2);
 
-// const outputsTypescript = `/* tslint:disable */\n\nexport const outputs = ${outputsJson}`;
+const outputsTypescript = `/* tslint:disable */\n\nexport const outputs = ${outputsJson}`;
 
-// fs.writeFileSync(`${OUTPUTS_PATH}/outputs.json`, outputsJson);
-// fs.writeFileSync(`${OUTPUTS_PATH}/outputs.ts`, outputsTypescript);
+fs.writeFileSync(`${OUTPUTS_PATH}/outputs.json`, outputsJson);
+fs.writeFileSync(`${OUTPUTS_PATH}/outputs.ts`, outputsTypescript);

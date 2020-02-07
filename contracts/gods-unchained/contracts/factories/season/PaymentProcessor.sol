225d38399188acdc1d5efb94eec501c87852ffba
Kerman Kohli <kermankohli@gmail.com>
2020-02-07 18:25:25 +1100

    Payment processor created with test cases outlined

--- contracts/gods-unchained/contracts/factories/season/PaymentProcessor.sol
+++ contracts/gods-unchained/contracts/factories/season/PaymentProcessor.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.11;
 
 contract PaymentProcessor {
 
-    mapping public (address => bool) sellersMapping;
+    mapping (bytes32 => address[]) public approvedSellers;
 
     address public sellersArray;
 
@@ -11,19 +11,19 @@ contract PaymentProcessor {
     address public revenueCollector;
 
     event RecordPurchase(
-      address from,
-      address user,
-      uint256 purchaseId,
-      bytes32 sku,
-      uint256 itemCost,
-      uint256 itemsCount,
-      address paymentToken,
-      address revenueCollector,
-      address[] affiliates,
-      uint256[] affiliateAmounts
+        address user,
+        uint256 purchaseId,
+        bytes32 sku,
+        uint256 itemCost,
+        uint256 itemsCount,
+        address paymentToken,
+        address revenueCollector,
+        address[] affiliates,
+        uint256[] affiliateAmounts
     );
 
-    constructor() public {};
+    event SellerApproved();
+    event SellerDisapproved();
 
     /**
      * @dev Process a payment made and handle referrals as well
@@ -42,33 +42,40 @@ contract PaymentProcessor {
         uint256 _itemCost,
         uint256 _itemsCount,
         address _paymentToken,
-        address[] _affiliates,
-        uint256[] _affiliateAmounts
-    ) public returns (uint256) {};
+        address[] memory _affiliateAddresses,
+        uint256[] memory _affiliateAmounts
+    ) public returns (uint256) {}
 
     /**
      * @dev Get the approved sellers who can process payments from this contract.
      *
+     * @param _code The SKU code to get thes sellers for
      */
-    function getSellers() public view returns (address[]) {};
+    function getSellers(
+        bytes32 _code
+    ) public view returns (address[] memory) {}
 
     /**
      * @dev Add a new seller who can process payments.
      * 
      * @param _seller Address of the seller to add
+     * @param _code SKU code they can purchase against
     */
-    function addSeller(
-        address _seller
-    ) public {};
+    function addSkuCode(
+        address _seller,
+        bytes32 _code
+    ) public {}
 
     /**
      * @dev Remove an existing seller who can process payments
      * WARNING: this could cause purchases to fail if still being used.
      *
      * @param _seller Address of the seller to remove
+     * @param _code SKU code they cannot purchase against
      */
-    function removeSeller(
-        address _seller
-    ) public {};
+    function removeSkuCode(
+        address _seller,
+        bytes32 _code
+    ) public {}
 
 }

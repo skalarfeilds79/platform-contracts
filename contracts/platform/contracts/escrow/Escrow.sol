1cf3669e840ce558692469d9fa7b440567642727
Alex Connolly <alex@immutable.com>
2020-02-07 11:36:43 +1100

    adding rough documentation, cleaning unnecessary events, updated formatting, switching from account array to mapping

--- contracts/platform/contracts/escrow/Escrow.sol
+++ contracts/platform/contracts/escrow/Escrow.sol
@@ -1,66 +1,110 @@
 pragma solidity ^0.6.1;
 
-import "./Asset.sol";
+import "../Asset.sol";
 
 contract Escrow {
 
-    // Emitted when a range of tokens is locked 
-    event RangeEscrowed(uint id);
-    // Emitted when a list of tokens is locked
-    event ListEscrowed(uint id);
+    // Emitted when a range of tokens is escrowed 
+    event RangeEscrowed(
+        uint indexed id,
+        address indexed owner,
+        address indexed asset, 
+        uint low, 
+        uint high,
+        address releaser
+    );
 
-    // Emitted when a range of tokens is released
-    event RangeReleased(uint id);
-    // Emitted when a list of tokens is released
-    event ListReleased(uint id);
+    // Emitted when a list of tokens is escrowed
+    event ListEscrowed(
+        uint id
+        address indexed owner,
+        address indexed asset, 
+        uint[] ids,
+        address releaser
+    );
 
-    // Emitted when a range of tokens is rescinded
-    event RangeRescinded(uint id);
-    // Emitted when a list of tokens is rescinded
-    event ListRescinded(uint id);
+    // Emitted when an account is released
+    event Released(
+        uint id
+    );
 
     struct Account {
-        
         // the address which will own these assets during escrow
         address owner;
         // the address authorised to release these assets from escrow
         address releaser;
-
         // The contract address of the asset 
         Asset asset;
-        // the high and low of the range
-        uint high;
+        // The low end id of the range in escrow
         uint low;
+        // The high end id of the range in escrow
+        uint high;
         // the list of the token ids in escrow. If the length of this array is 0, a range has been escrowed
         uint[] ids;
     }
 
-    // All escrow accounts 
-    // TODO: should old escrow accounts be cleared? If so, how to structue?
-    Account[] public Account;
+    // Count of how many escrow accounts have been created
+    uint public counter;
+    // Mapping from id to escrow accounts, used rather than an array to enable deleting released accounts
+    mapping(uint => Account) accounts;
 
+    /**
+     * @dev Escrow a consecutive range of assets
+     *
+     * @param _asset NFT contract address of the assets to be escrowed
+     * @param _low The first token ID in the range to be escrowed (inclusive)
+     * @param _high The last token ID in the range to be escrowed (inclusive)
+     * @param _owner Address which will own these assets during escrow
+     * @param _releaser Address which can release these assets from escrow
+     */
     function escrowRange(
-        Asset _asset, uint _low, uint _high,
-        address _owner, address _releaser
+        Asset _asset, 
+        uint _low, 
+        uint _high,
+        address _owner, 
+        address _releaser
     ) public returns (uint) {
 
     }
 
+    /**
+     * @dev Escrow a list of assets
+     *
+     * @param _asset NFT contract address of the assets to be escrowed
+     * @param _ids Token IDs of the cards to be escrowed
+     * @param _owner Address which will own these assets during escrow
+     * @param _releaser Address which can release these assets from escrow
+     */
     function escrowList(
-        Asset _asset, uint[] _ids,
-        address _owner, address _releaser
+        Asset _asset, 
+        uint[] _ids,
+        address _owner, 
+        address _releaser
     ) public returns (uint) {
-
-    }
-
-    function release(uint _id, address _to) public {
         
     }
 
-    function _buildList(uint low, uint high) internal returns (uint[] memory) {
+    /**
+     * @dev Release cards from escrow. Can only be called by the account's releaser. 
+     *
+     * @param _id ID of the escrow account
+     * @param _to Address to which the cards should be transferred
+     */
+    function release(
+        uint _id, 
+        address _to
+    ) public {
+        
+    }
+
+    function _buildList(
+        uint low, 
+        uint high
+    ) internal returns (uint[] memory) {
         uint length = high - low;
         uint[] memory ids = uint[](length);
-        for (uint i = 0; i < length; i++) {
+        // <= as the range is inclusive a
+        for (uint i = 0; i <= length; i++) {
             ids[i] = low + i;
         }
         return ids;

dcc8de092904bc77ac934c431a4b616a223f8bb2
Alex Connolly <alex@immutable.com>
2020-03-22 23:55:33 +1100

    adding return type for commit

--- contracts/platform/contracts/randomness/IBeacon.sol
+++ contracts/platform/contracts/randomness/IBeacon.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.11;
 
 contract IBeacon {
 
-    function commit(uint256 offset) public;
+    function commit(uint256 offset) public returns (uint256);
     function recommit(uint256 commitBlock, uint256 offset) public;
     function randomness(uint64 commitBlock) public view returns (bytes32);
     function callback(uint256 commitBlock) public;

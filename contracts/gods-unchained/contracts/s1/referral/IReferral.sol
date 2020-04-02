444111d9d50096499b6d8ce9ac9196103446b91d
Alex Connolly <alex@immutable.com>
2020-04-02 22:33:12 +1100

    matching style guide

--- contracts/gods-unchained/contracts/s1/referral/IReferral.sol
+++ contracts/gods-unchained/contracts/s1/referral/IReferral.sol
@@ -2,8 +2,15 @@ pragma solidity 0.5.11;
 
 interface IReferral {
 
+    /**
+     * @dev Get the split of cost between purchaser/referrer
+     *
+     * @param _user The address of the purchasing user
+     * @param _value The total value of the purchase
+     * @param _referrer The address of the referring user
+     */
     function getSplit(
-        address user, uint256 value, address referrer
+        address _user, uint256 _value, address _referrer
     ) external view returns (
         uint256 toVendor, uint256 toReferrer
     );

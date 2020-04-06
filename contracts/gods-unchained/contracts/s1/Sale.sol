acfba3520a41b7c5daa1032698d361e8f715019c
Alex Connolly <alex@immutable.com>
2020-04-06 21:58:22 +1000

    updating sale contract types

--- contracts/gods-unchained/contracts/s1/Sale.sol
+++ contracts/gods-unchained/contracts/s1/Sale.sol
@@ -7,11 +7,29 @@ contract Sale {
 
     /** @dev Purchase assets from a number of products
      *
-     * @param _user the user who will receive the assets
-     * @param _referrer the address of the user who referred this purchase
      * @param _products the products this user is purchasing
      * @param _quantities the quantities this user is purchasing
      * @param _payments the details of the method by which payments will be made
+     * @param _referrer the address of the user who referred this purchase
+     */
+    function purchase(
+        Product[] memory _products,
+        uint256[] memory _quantities,
+        IPay.Payment[] memory _payments,
+        address payable _referrer
+    ) public {
+        for (uint i = 0; i < _products.length; i++) {
+            _products[i].purchase(_quantities[i], _payments[i], _referrer);
+        }
+    }
+
+    /** @dev Purchase assets from a number of products
+     *
+     * @param _user the user who will receive the assets
+     * @param _products the products this user is purchasing
+     * @param _quantities the quantities this user is purchasing
+     * @param _payments the details of the method by which payments will be made
+     * @param _referrer the address of the user who referred this purchase
      */
     function purchaseFor(
         address payable _user,

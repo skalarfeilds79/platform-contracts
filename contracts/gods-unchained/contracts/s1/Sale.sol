84982ddb1c68c7fe383ee10b2a5405f1313915e6
Alex Connolly <alex@immutable.com>
2020-04-11 19:17:33 +1000

    removing sorted arrays requirement

--- contracts/gods-unchained/contracts/s1/Sale.sol
+++ contracts/gods-unchained/contracts/s1/Sale.sol
@@ -1,43 +1,51 @@
 pragma solidity 0.5.11;
 pragma experimental ABIEncoderV2;
 
-import "./Product.sol";
+import "./IProduct.sol";
+import "@imtbl/platform/contracts/pay/IPay.sol";
 
 contract Sale {
 
+    struct Purchase {
+        // the product this user is purchasing
+        IProduct product;
+        // the quantity this user is purchasing
+        uint256 quantity;
+        // the details of the method by which payments will be made
+        IPay.Payment payment;
+    }
+
     /** @dev Purchase assets from a number of products
      *
-     * @param _products the products this user is purchasing
-     * @param _quantities the quantities this user is purchasing
-     * @param _payments the details of the method by which payments will be made
+     * @param _purchases details of the products being purchased
      * @param _referrer the address of the user who referred this purchase
      */
     function purchase(
-        Product[] memory _products,
-        uint256[] memory _quantities,
-        IPay.Payment[] memory _payments,
+        Purchase[] memory _purchases,
         address payable _referrer
     ) public payable {
-        purchaseFor(msg.sender, _products, _quantities, _payments, _referrer);
+        purchaseFor(msg.sender, _purchases, _referrer);
     }
 
     /** @dev Purchase assets from a number of products
      *
      * @param _recipient the user who will receive the assets
-     * @param _products the products this user is purchasing
-     * @param _quantities the quantities this user is purchasing
-     * @param _payments the details of the method by which payments will be made
+     * @param _purchases details of the products being purchased
      * @param _referrer the address of the user who referred this purchase
      */
     function purchaseFor(
         address payable _recipient,
-        Product[] memory _products,
-        uint256[] memory _quantities,
-        IPay.Payment[] memory _payments,
+        Purchase[] memory _purchases,
         address payable _referrer
     ) public payable {
-        for (uint i = 0; i < _products.length; i++) {
-            _products[i].purchaseFor(_recipient, _quantities[i], _payments[i], _referrer);
+        for (uint i = 0; i < _purchases.length; i++) {
+            Purchase memory p = _purchases[i];
+            p.product.purchaseFor(
+                _recipient,
+                p.quantity,
+                p.payment,
+                _referrer
+            );
         }
     }
 }

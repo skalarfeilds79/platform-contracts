c798473baccf46ee993a9a20bef97abe3d0466bd
Dane Lipscombe <dane.lipscombe@immutable.com>
2020-05-13 09:22:39 +1000

    Combine cici jobs

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -2,17 +2,8 @@
 
 set -euxo pipefail
 
+cp .env.default .env
 docker-compose up -d
-yarn install
-yarn lerna bootstrap
-yarn setup
-yarn lint
-docker-compose logs
-docker-compose ps
-export ETHERSCAN_KEY=0
-export RPC_ENDPOINT=http://localhost:8545
-export DEPLOYMENT_NETWORK_ID=50
-export DEPLOYMENT_ENVIRONMENT=development
-env
-yarn test
+sleep 5
+scripts/build-tools.sh docker-run test
 docker-compose down -v

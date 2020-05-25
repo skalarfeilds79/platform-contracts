578aed812736a0220c8f47141ccd70f5609c51f9
Alex Connolly <alex@immutable.com>
2020-05-25 10:40:40 +1000

    more

--- packages/deployment-utils/src/stage.ts
+++ packages/deployment-utils/src/stage.ts
@@ -1,10 +1,12 @@
+import { DeploymentParams } from './params';
 
 export abstract class DeploymentStage {
-  constructor(privateKey: string, rpcUrl: string, networkId: number) {}
+
+  constructor(params: DeploymentParams) {}
 
   async deploy(
     findInstance: (name: string) => Promise<string>,
     onDeployment: (name: string, address: string, dependency: boolean) => void,
-    transferOwnership: (address: string) => void,
+    transferOwnership: (addresses: string[]) => void,
   ) {}
 }

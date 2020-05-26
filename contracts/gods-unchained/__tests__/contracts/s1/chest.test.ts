0cc407829e48233907b3313b7219ab782388c140
Alex Connolly <alex@immutable.com>
2020-05-26 17:42:27 +1000

    continued

--- contracts/gods-unchained/__tests__/contracts/s1/chest.test.ts
+++ contracts/gods-unchained/__tests__/contracts/s1/chest.test.ts
@@ -24,38 +24,38 @@ describe('Chest', () => {
     await blockchain.revertAsync();
   });
 
-  // describe('#purchase ETH', () => {
+  describe('#purchase ETH', () => {
 
-  //   let shared: StandardContracts;
-  //   let rare: RarePack;
-  //   let chest: Chest;
+    let shared: StandardContracts;
+    let rare: RarePack;
+    let chest: Chest;
 
-  //   beforeAll(async() => {
-  //     shared = await deployStandards(owner);
-  //   });
+    beforeAll(async() => {
+      shared = await deployStandards(owner);
+    });
 
-  //   beforeEach(async () => {
-  //     rare = await deployRarePack(owner, shared);
-  //     chest = await deployRareChest(owner, rare, shared);
-  //   });
+    beforeEach(async () => {
+      rare = await deployRarePack(owner, shared);
+      chest = await deployRareChest(owner, rare, shared);
+    });
 
-  //   async function purchaseChests(quantity: number) {
-  //     let balance = await chest.balanceOf(owner.address);
-  //     expect(balance.toNumber()).toBe(0);
-  //     const ethRequired = await shared.oracle.convert(1, 0, GU_S1_RARE_CHEST_PRICE * quantity);
-  //     await chest.purchase(quantity, getETHPayment(), ethers.constants.AddressZero, { value: ethRequired});
-  //     balance = await chest.balanceOf(owner.address);
-  //     expect(balance.toNumber()).toBe(quantity);
-  //   }
+    async function purchaseChests(quantity: number) {
+      let balance = await chest.balanceOf(owner.address);
+      expect(balance.toNumber()).toBe(0);
+      const ethRequired = await shared.oracle.convert(1, 0, GU_S1_RARE_CHEST_PRICE * quantity);
+      await chest.purchase(quantity, getETHPayment(), ethers.constants.AddressZero, { value: ethRequired});
+      balance = await chest.balanceOf(owner.address);
+      expect(balance.toNumber()).toBe(quantity);
+    }
 
-  //   it('should purchase 1 chest using ETH', async() => {
-  //     await purchaseChests(1);
-  //   });
+    it('should purchase 1 chest using ETH', async() => {
+      await purchaseChests(1);
+    });
 
-  //   it('should purchase 5 chests using ETH', async() => {
-  //     await purchaseChests(5);
-  //   });
-  // });
+    it('should purchase 5 chests using ETH', async() => {
+      await purchaseChests(5);
+    });
+  });
 
   describe('#purchase USD', () => {
 
@@ -79,7 +79,7 @@ describe('Chest', () => {
         quantity,
         sku: GU_S1_RARE_CHEST_SKU,
         assetRecipient: owner.address,
-        changeRecipient: owner.address,
+        changeRecipient: chest.address,
         currency: Currency.USDCents,
         totalPrice: value,
         alreadyPaid: 0
@@ -93,28 +93,28 @@ describe('Chest', () => {
         params,
       );
       await chest.purchase(quantity, payment, ethers.constants.AddressZero);
-      // let expectedUserBalance: number;
-      // let expectedEscrowBalance: number;
-      // if (escrowFor > 0) {
-      //   expectedUserBalance = 0;
-      //   expectedEscrowBalance = quantity;
-      // } else {
-      //   expectedUserBalance = quantity;
-      //   expectedEscrowBalance = 0;
-      // }
-      // const escrowBalance = await chest.balanceOf(shared.escrow.address);
-      // const userBalance = await chest.balanceOf(owner.address);
-      // expect(userBalance.toNumber()).toBe(expectedUserBalance);
-      // expect(escrowBalance.toNumber()).toBe(expectedEscrowBalance);
+      let expectedUserBalance: number;
+      let expectedEscrowBalance: number;
+      if (escrowFor > 0) {
+        expectedUserBalance = 0;
+        expectedEscrowBalance = quantity;
+      } else {
+        expectedUserBalance = quantity;
+        expectedEscrowBalance = 0;
+      }
+      const escrowBalance = await chest.balanceOf(shared.escrow.address);
+      const userBalance = await chest.balanceOf(owner.address);
+      expect(userBalance.toNumber()).toBe(expectedUserBalance);
+      expect(escrowBalance.toNumber()).toBe(expectedEscrowBalance);
     }
 
-    // it('should purchase 1 chest using USD with no escrow', async () => {
-    //   await purchaseChests(1, 0);
-    // });
+    it('should purchase 1 chest using USD with no escrow', async () => {
+      await purchaseChests(1, 0);
+    });
 
-    // it('should purchase 5 chests using USD with no escrow', async () => {
-    //   await purchaseChests(5, 0);
-    // });
+    it('should purchase 5 chests using USD with no escrow', async () => {
+      await purchaseChests(5, 0);
+    });
 
     it('should purchase 1 chest using USD with escrow', async () => {
       await purchaseChests(1, 10000);
@@ -125,45 +125,45 @@ describe('Chest', () => {
     });
   });
 
-  // describe('#open', () => {
+  describe('#open', () => {
 
-  //   let shared: StandardContracts;
-  //   let rare: RarePack;
-  //   let chest: Chest;
+    let shared: StandardContracts;
+    let rare: RarePack;
+    let chest: Chest;
 
-  //   beforeAll(async() => {
-  //     shared = await deployStandards(owner);
-  //   });
+    beforeAll(async() => {
+      shared = await deployStandards(owner);
+    });
 
-  //   beforeEach(async () => {
-  //     rare = await deployRarePack(owner, shared);
-  //     chest = await deployRareChest(owner, rare, shared);
-  //   });
+    beforeEach(async () => {
+      rare = await deployRarePack(owner, shared);
+      chest = await deployRareChest(owner, rare, shared);
+    });
 
-  //   async function openChests(quantity: number) {
-  //     const preBalance = await chest.balanceOf(owner.address);
-  //     await chest.open(quantity);
-  //     const postBalance = await chest.balanceOf(owner.address);
-  //     const diff = preBalance.toNumber() - postBalance.toNumber();
-  //     expect(diff).toBe(quantity);
-  //   }
+    async function openChests(quantity: number) {
+      const preBalance = await chest.balanceOf(owner.address);
+      await chest.open(quantity);
+      const postBalance = await chest.balanceOf(owner.address);
+      const diff = preBalance.toNumber() - postBalance.toNumber();
+      expect(diff).toBe(quantity);
+    }
 
-  //   it('should not be able to open chests with insufficient balance', async () => {
-  //     await expectRevert(chest.open(1));
-  //   });
+    it('should not be able to open chests with insufficient balance', async () => {
+      await expectRevert(chest.open(1));
+    });
 
-  //   it('should be able to open 1 chest', async () => {
-  //     const ethRequired = await shared.oracle.convert(1, 0, GU_S1_RARE_CHEST_PRICE);
-  //     const payment = getETHPayment();
-  //     await chest.purchase(1, payment, ethers.constants.AddressZero, { value: ethRequired });
-  //     await openChests(1);
-  //   });
+    it('should be able to open 1 chest', async () => {
+      const ethRequired = await shared.oracle.convert(1, 0, GU_S1_RARE_CHEST_PRICE);
+      const payment = getETHPayment();
+      await chest.purchase(1, payment, ethers.constants.AddressZero, { value: ethRequired });
+      await openChests(1);
+    });
 
-  //   it('should be able to open 5 chests', async () => {
-  //     const ethRequired = await shared.oracle.convert(1, 0, GU_S1_RARE_CHEST_PRICE * 5);
-  //     const payment = getETHPayment();
-  //     await chest.purchase(5, payment, ethers.constants.AddressZero, { value: ethRequired });
-  //     await openChests(5);
-  //   });
-  // });
+    it('should be able to open 5 chests', async () => {
+      const ethRequired = await shared.oracle.convert(1, 0, GU_S1_RARE_CHEST_PRICE * 5);
+      const payment = getETHPayment();
+      await chest.purchase(5, payment, ethers.constants.AddressZero, { value: ethRequired });
+      await openChests(5);
+    });
+  });
 });

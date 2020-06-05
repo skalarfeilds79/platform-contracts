a333c25a20268e16ae3821f2b47192647f4c82dc
Dane Lipscombe <dane.lipscombe@immutable.com>
2020-06-05 16:53:16 +1000

    Make environment dynamic

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,15 +3,27 @@ const fs = require('fs');
 
 const platform = JSON.parse(fs.readFileSync(__dirname + '/../../../contracts/platform/src/addresses/addresses.json', 'utf8'));
 const godsUnchained = JSON.parse(fs.readFileSync(__dirname + '/../../../contracts/gods-unchained/src/addresses/addresses.json', 'utf8'));
+
 const port = process.env.PORT || 3000;
+const environment = process.env.DEPLOYMENT_ENVIRONMENT || "development";
+if (!platform.environments.hasOwnProperty(environment)) {
+  throw new Error(`Platform addresses missing environment ${environment}`);
+}
+
+if (!godsUnchained.environments.hasOwnProperty(environment)) {
+  throw new Error(`Gods Unchained addresses missing environment ${environment}`);
+}
+
+const platformData = platform.environments[environment];
+const godsUnchainedData = godsUnchained.environments[environment];
 
 const app = express();
 app.get('/addresses/platform', (request, response) => {
-  response.json(platform.environments.development);
+  response.json(platformData);
 });
 
 app.get('/addresses/gods-unchained', (request, response) => {
-  response.json(godsUnchained.environments.development);
+  response.json(godsUnchainedData);
 });
 
 console.log(`Server listening on port ${port}`)

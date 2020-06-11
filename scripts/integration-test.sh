86f0ef534ea569ea003cb74c51e5c456bbd59070
Dane Lipscombe <dane.lipscombe@immutable.com>
2020-06-11 17:54:17 +1000

    Fix command

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 cp .env.example .env
 docker-compose up -d
 sleep 5
-scripts/build-tools.sh docker-run run-with-env deploy:all
+scripts/build-tools.sh docker-run run-with-env yarn deploy:all
 scripts/build-tools.sh docker-run test
 docker-compose down -v
